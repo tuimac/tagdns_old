403f1b89a3a3679ba410a92e0e58d7d73be81b0b
tuimac <tuimac.devadm01@gmail.com>
2019-11-25 12:53:32 +0900

    under development

--- dns.py
+++ dns.py
@@ -1,6 +1,9 @@
 import socket
 import threading
+##watch this article
+# https://stackoverflow.com/questions/4373728/can-more-than-one-thread-use-the-same-port
 import re
+import queue
 
 REGEX = "^(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)$"
 
@@ -16,4 +19,5 @@ class Dns():
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((self.ip, self.port))
         data, addr = sock.recvfrom(512)
-        print(data)
+        print(addr)
+        return data

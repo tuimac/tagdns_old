806eeb08dd9cd4859f4d4a8127d6271686d631c7
tuimac <tuimac.devadm01@gmail.com>
2019-12-02 23:45:26 +0900

    under development

--- getRequest.py
+++ getRequest.py
@@ -1,14 +1,13 @@
 from threading import Thread
 
-
 class GetRequest(Thread):
     def __init__(self, queue):
         self.queue = queue
         
     def workerNode(self, self.queue):
-        while True:
-            
-            
+        while self.queue.qsize() > 0:
+
+
 
     def createThread(self):
         

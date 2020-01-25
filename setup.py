7a4377ba3772860a156683a02ed09c7e568e44f8
tuimac <tuimac.devadm01@gmail.com>
2020-01-25 14:05:24 +0900

    under development

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from setuptools import setup, find_packeges
+from setuptools import setup, find_packages
+import sys
+import os
+
+CONFIG = "/etc/tagdns"
+
+if os.path.exists(CONFIG) is False:
+    os.mkdir(CONFIG)
+    print(os.getcwd())
 
 setup(
     name="tagdns",
@@ -10,15 +18,20 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8'
     ],
     description="Dynamic DNS for Amazon EC2",
-    python_requires='>=3.6.0'
-    packages=find_packeges(where=src),
-    package_dir={'': 'src'},
+    python_requires='>=3.6.0',
+    packages=find_packages(where='tagdns'),
+    package_dir={'': 'tagdns'},
     install_requires=[
         "boto3>=1.9.0",
     ],
+    entry_points={
+        'console_scripts'=[
+            'tagdns = src.main:run'
+    }
 )

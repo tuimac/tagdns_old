4fed034e715bdc9040b2eb3770e69d1e4796d147
tuimac <tuimac.devadm01@gmail.com>
2020-02-02 17:26:25 +0900

    under development

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
     description="Dynamic DNS for Amazon EC2",
     python_requires=">=3.6.0",
     packages=find_packages(),
-    package_dir={"": "tagdns"},
     include_package_data=True,
     zip_safe=False,
     install_requires=[

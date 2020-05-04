96f1108f4ba76163f2417c42f1bef980622bf0c5
tuimac <tuimac.devadm01@gmail.com>
2020-05-04 15:54:41 +0900

    under development

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -2,6 +2,15 @@
 
 target="jenkins"
 
-docker build -t tagdns /home/tuimac/github/tagdns/docker/${target}
-docker run -d -it --name ${target} $target /bin/bash
+cd jenkins
+
+docker build -t $target /home/tuimac/github/tagdns/docker/${target}
+docker run -itd \
+    --volume volume:/tmp \
+    --publish 8000:8080 \
+    --publish 50000:50000 \
+    --name $target \
+    --network=br0 \
+    --tty yes \
+    $target /bin/bash
 docker start $target

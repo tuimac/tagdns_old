2cfb2e60e3187a3bc8eac42e7109ba51dd7f7fe0
tuimac <tuimac.devadm01@gmail.com>
2020-05-05 18:29:41 +0900

    under development

--- docker/tagdns/deploy.sh
+++ docker/tagdns/deploy.sh
@@ -4,7 +4,6 @@ target="tagdns"
 
 docker build -t $target ${HOME}/tagdns/docker/${target}
 docker container create -it \
-    --publish 53:53 \
     --name $target \
     --network=br0 \
     $target /bin/bash

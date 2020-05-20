8fb1a9ea0ec3953655a5efa307e877958686cc5d
tuimac <tuimac.devadm01@gmail.com>
2020-05-20 23:06:56 +0900

    under development

--- docker/jenkins/entrypoint.sh
+++ docker/jenkins/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-#cat /root/tagdns/docker/jenkins/tagdns-config.xml | java -jar /root/tagdns/docker/jenkins/jenkins-cli.jar -s http://localhost:8080 create-job tagdns
+sleep 5
+
+cat /root/tagdns/docker/jenkins/tagdns-config.xml | java -jar /root/tagdns/docker/jenkins/jenkins-cli.jar -s http://localhost:8080 create-job tagdns

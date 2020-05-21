5cfa285cc5f186a07acb73ebf8aefc6510090717
tuimac <tuimac.devadm01@gmail.com>
2020-05-21 22:48:31 +0900

    under development

--- docker/jenkins/command.sh
+++ docker/jenkins/command.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sleep 5
-
 cat /root/tagdns/docker/jenkins/tagdns-config.xml | java -jar /root/tagdns/docker/jenkins/jenkins-cli.jar -s http://localhost:8080 create-job tagdns
+
+# Access "http://localhost:8080/script"
+
+Jenkins.instance.pluginManager.plugins.each{
+  plugin -> 
+    println ("${plugin.getShortName()}")
+}

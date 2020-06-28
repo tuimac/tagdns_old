d23dd771b3b81919970c6f187aa7c55cf390740a
tuimac <tuimac.devadm01@gmail.com>
2020-06-29 00:14:31 +0900

    create new

--- docker/jenkins/command.sh
+++ docker/jenkins/command.sh
@@ -2,6 +2,9 @@
 
 cat /root/tagdns/docker/jenkins/tagdns-config.xml | java -jar /root/tagdns/docker/jenkins/jenkins-cli.jar -s http://localhost:8080 create-job tagdns
 
+java -jar jenkins-cli.jar -s http://node-master:8000 get-job tagdns > tagdns-config.xml
+
+
 # Access "http://localhost:8080/script"
 
 Jenkins.instance.pluginManager.plugins.each{

0a2cb1a96cdd2c1fef6833313603020e798e8c78
tuimac <tuimac.devadm01@gmail.com>
2020-07-06 11:41:09 +0900

    delete create directory for test

--- test/unit/test_endpoint.py
+++ test/unit/test_endpoint.py
@@ -24,7 +24,7 @@ class TestEndpoint(unittest.TestCase):
 
 if __name__ == '__main__':
     if not os.path.exists(REPORT):
-        os.mkdir(os.path.dirname(REPORT))
+        #os.mkdir(os.path.dirname(REPORT))
         with open(REPORT, 'a') as f:
             f.write("")
     with open(REPORT, 'wb') as output:

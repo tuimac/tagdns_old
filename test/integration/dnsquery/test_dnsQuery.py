6d355e30c9f092a5b4deaa98a9769ee40c5a4876
tuimac <tuimac.devadm01@gmail.com>
2020-07-09 11:56:19 +0900

    fixed

---
+++ test/integration/dnsquery/test_dnsQuery.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from dns import resolver, reversename
+from os import path, mkdir
+import unittest
+import socket
+import xmlrunner
+import traceback
+import sys
+
+REPORT = 'reports/tagdns_' + path.basename(__file__).split('.')[0] + '.xml'
+NAMESERVER = ''
+
+class TestDnsQuery(unittest.TestCase):
+    def test_queryA(self):
+        client = resolver.Resolver()
+        client.nameserver = [NAMESERVER]
+        result = client.query('tagdns','A')
+        ipaddress = result.rrset.items[0].address
+        self.assertEqual(nameserver, ipaddress)
+
+    def test_queryPTR(self):
+        ptr = reversename.from_address(NAMESERVER)
+        client = resolver.Resolver()
+        client.nameserver = [NAMESERVER]
+        results = client.query(ptr,'PTR')
+        self.assertNotEqual(0, results.len())
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        raise Exception('Need argument.')
+    else:
+        NAMESERVER = sys.argv[1]
+    if not path.exists(path.dirname(REPORT)):
+        mkdir(path.dirname(REPORT))
+        with open(REPORT, 'a') as f:
+            f.write('')
+    with open(REPORT, 'wb') as output:
+        unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output))
---
+++ test/unit/test_endpoint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from os import path, mkdir
+import sys
+import time
+import traceback
+import unittest
+import xmlrunner
+
+sys.path.insert(0, path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+from tagdns.endpoint import Endpoint
+
+REPORT = path.dirname(path.dirname(path.abspath(__file__))) + '/reports/tagdns_' + path.basename(__file__).split('.')[0] + '.xml'
+
+class TestEndpoint(unittest.TestCase):
+    def test_closeEndpoint(self):
+        try:
+            endpoint = Endpoint()
+            endpoint.daemon = True
+            endpoint.start()
+            endpoint.closeAllEndpoint()
+            time.sleep(1)
+        except:
+            self.fail(traceback.format_exc())
+
+if __name__ == '__main__':
+    if not path.exists(path.dirname(REPORT)):
+        mkdir(path.dirname(REPORT))
+        with open(REPORT, 'a') as f:
+            f.write('')
+    with open(REPORT, 'wb') as output:
+        unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output))

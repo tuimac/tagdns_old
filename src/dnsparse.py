3da92c49cfcbf9895904813068c67701838b689c
tuimac <tuimac.devadm01@gmail.com>
2020-07-18 16:49:26 +0900

    fixed

--- src/dnsparse.py
+++ src/dnsparse.py
@@ -1,9 +1,27 @@
 import struct
+import json
 
 class DNSParse:
-    @staticmethod
-    def a(query):
-        #id16, qr1, opcode4, aa1, tc1, rd1, ra1, z1, ad1, cd1, rcode4 
+    def __init__(self, query):
+        self.query = dict()
+        self.query['id'], flags, self.query['qdcount'], self.query['ancount'], self.query['nscount'], self.query['arcount'] = struct.unpack('!HHHHHH', query[:12])
+        self.query['qr'] = (flags & 32768) >> 15
+        self.query['opcode'] = (flags & 30720) >> 11
+        self.query['aa'] = (flags & 1024) >> 10
+        self.query['tc'] = (flags & 512) >> 9
+        self.query['rd'] = (flags & 256) >> 8
+        self.query['ra'] = (flags & 128) >> 7
+        self.query['z'] = (flags & 112) >> 4
+        self.query['rcode'] = flags & 15
+        self.dump()
+
+    def a(self, query):
         datalist = struct.unpack('', query[:8])
         for x in datalist:
             print(x)
+
+    def dump(self):
+        print(json.dumps(self.query,
+            indent=4,
+            separators=(',', ': ')
+        ))
--- src/driver.py
+++ src/driver.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import struct
-from dnspaarse import DNSParse
+from dnsparse import DNSParse
 
 if __name__ == '__main__':
-    query = (b'8\xf7\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x04test\x06tuimac\x07private\x00\x00\x01\x00\x01', ('172.17.0.2', 55909))
-    data = query[0]
-    DNSParse.a()
+    queries = [
+        (b'8\xf7\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x04test\x06tuimac\x07private\x00\x00\x01\x00\x01', ('172.17.0.2', 55909)),
+        (b'\xc1\xd0\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x012\x010\x0217\x03172\x07in-addr\x04arpa\x00\x00\x0c\x00\x01', ('172.17.0.2', 44703)),
+    ]
+    for query in queries:
+        dnsparse = DNSParse(query[0])
